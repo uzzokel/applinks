d5c3e6e2c9bc74f868414518e3faea5c452604a6
Uzoamaka Ojukwu <uzzokel@gmail.com>
2026-07-03 12:45:24 +0100

    updates

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaNeon } from '@prisma/adapter-neon' // Removed Pool import
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaClient } from '../app/generated/prisma'
 
 const connectionString = 
   "postgresql://neondb_owner:npg_5qHQwlRJxzu8@ep-spring-pond-apr5wuvf-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&pgbouncer=true";
@@ -8,6 +8,7 @@ function getPrismaClient() {
   // Pass the connectionString object directly to PrismaNeon instead of a Pool instance
   const adapter = new PrismaNeon({ connectionString })
   
+  // Fixed: Instantiating directly from the imported PrismaClient class
   return new PrismaClient({ adapter })
 }
 
